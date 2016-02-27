b4a916d3d42de987e67c1b9cc208de8b40124bd3
Souphan <souphan.ctr@gmail.com>
2016-02-26 23:04:56 -0600

    added navMenu directive and firebase backend

--- dist/scripts/Controllers/JobPostCtrl.js
+++ dist/scripts/Controllers/JobPostCtrl.js
@@ -1,9 +1,102 @@
-var appAngular = angular.module("blocJobs");
-
-appAngular.controller("JobPostCtrl", function($scope) {      
+blocJobs.controller('jobPostCtrl', ['$scope', '$firebaseArray', function($scope, $firebaseArray) {  
+    
     $scope.myVar = false;
     $scope.toggle = function() {
         $scope.myVar = !$scope.myVar;
     };
-      
-});
+    
+ $scope.safeApply = function(fn) {
+        var phase = this.$root.$$phase;
+        if(phase == '$apply' || phase == '$digest') {
+            if(fn && (typeof(fn) === 'function')) {
+                fn();
+            }
+        } else {
+            this.$apply(fn);
+        }
+    };
+    
+    //CREATE A FIREBASE REFERENCE
+    $scope.items = [];
+    var firebase = new Firebase("https://keodo-todo-list.firebaseio.com/");
+    $scope.addItem = function () {
+        firebase.push({"content": $scope.itemContent})
+        $scope.itemContent = '';
+    };
+    $scope.removeItem = function () {
+        var item = this.item;
+        firebase.child(item[".key"]).remove();
+    };
+    firebase.on("child_added", function(addedSnap) {
+        var item = {
+            ".key": addedSnap.key(),
+            "content": addedSnap.val().content
+        };
+        $scope.safeApply(function() {
+            $scope.items.push(item);
+        });
+        firebase.child(addedSnap.key()).on("value", function(valueSnap) {
+            $scope.safeApply(function() {
+                if (valueSnap.val()) {
+                    item.content = valueSnap.val().content;
+                } else {
+                    var idx = -1;
+                    $scope.items.forEach(function(e, i) {
+                        if (e[".key"] == valueSnap.key()) {
+                            idx = i;
+                            return;
+                        }
+                    });
+                    if (idx > -1) {
+                        $scope.items.splice(idx, 1);
+                    }
+                }
+            });
+        });
+    });
+    $scope.$watch("items", function(newItems, oldItems) {
+        // TODO: How do I check which item is updated in a better way?
+        newItems.forEach(function(newItem) {
+            oldItems.forEach(function(oldItem) {
+                if (newItem[".key"] == oldItem[".key"] && newItem.content != oldItem.content) {
+                    firebase.child(newItem[".key"]).update({"content": newItem.content});
+                }
+            });
+        });
+    }, true);
+
+
+  
+}]);
+
+
+//var myApp = angular.module("myapp", ["firebase"]);
+//
+//myApp.controller("MyController", ["$scope", "$firebaseArray",
+//function($scope, $firebaseArray) {
+//  //CREATE A FIREBASE REFERENCE
+//  var ref = new Firebase("https://keodo-todo-list.firebaseio.com/");
+//
+//  // GET MESSAGES AS AN ARRAY
+//  $scope.messages = $firebaseArray(ref);
+//
+//  //ADD MESSAGE METHOD
+//  $scope.addMessage = function(e) {
+//
+//    //LISTEN FOR RETURN KEY
+//    if (e.keyCode === 13 && $scope.msg) {
+//      //ALLOW CUSTOM OR ANONYMOUS USER NAMES
+//      var name = $scope.name || "anonymous";
+//
+//      //ADD TO FIREBASE
+//      $scope.messages.$add({
+//        from: name,
+//        body: $scope.msg
+//      });
+//
+//      //RESET MESSAGE
+//      $scope.msg = "";
+//    }
+//  }
+//}
+//]);
--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -1,4 +1,5 @@
 var blocJobs = angular.module('blocJobs', ["ui.router", "firebase"])
+
 blocJobs.config(function($stateProvider, $urlRouterProvider){
     
     // For any unmatched url, send to /route1
@@ -7,12 +8,23 @@ blocJobs.config(function($stateProvider, $urlRouterProvider){
      $stateProvider
      .state('landing', {
          url: '/',
-         controller: 'LandingCtrl',
+         controller: 'mainCtrl',
          templateUrl: '/templates/landing.html'
      })
     .state('job-post', {
         url: "/job-post",
-        controller: 'JobPostCtrl',
+        controller: 'jobPostCtrl',
         templateUrl: "/templates/job-post.html"
     })
+    .state('postings', {
+        url: "/postings",
+        controller: 'postingsCtrl',
+        templateUrl: "/templates/postings.html"
+    })
 })
+
+blocJobs.directive("naviMenu", function() {
+    return {
+        template: '<nav ui-view class="navbar navbar-inverse"><div class="container-fluid"><div class="navbar-header"><a class="navbar-brand" ui-sref="landing">Bloc</a></div><ul class="nav navbar-nav"><li class="active"><a href="#">Frontend Web Development</a></li><li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Roadmaps <span class="caret"></span></a><ul class="dropdown-menu"><li><a href="#">Page 1-1</a></li><li><a href="#">Page 1-2</a></li><li><a href="#">Page 1-3</a></li></ul></li><li><a href="#">Messages</a></li><li><a href="#">Schedule</a></li></ul><ul class="nav navbar-nav navbar-right"><li><a href="#"><span class="glyphicon glyphicon-user"></span> Account</a></li><li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li></ul></div></nav>'
+    }
+});
