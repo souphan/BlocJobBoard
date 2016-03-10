90fc154c35ac75b230eede93583867ae61e0209e
Souphan <souphan.ctr@gmail.com>
2016-03-10 08:28:37 -0600

    added comments in code

--- dist/scripts/Controllers/JobPostCtrl.js
+++ dist/scripts/Controllers/JobPostCtrl.js
@@ -1,5 +1,6 @@
 blocJobs.controller('jobPostCtrl', ['$scope', '$firebaseArray', function($scope, $firebaseArray) {  
     
+    //TOGGLE RADIO BUTTON TO SHOW ADDRESS
     $scope.myVar = false;
     $scope.myVarTwo = false;
     $scope.toggle = function() {
@@ -10,26 +11,27 @@ blocJobs.controller('jobPostCtrl', ['$scope', '$firebaseArray', function($scope,
     $scope.myVarTwo = !$scope.myVarTwo;
     };
     
+    //SETTING VARIABLE FOR EMPTY TEXT INPUT
     $scope.jobTitle = "";
     $scope.companyName = "";
     $scope.jobDescription = "";
-//    $scope.fullTime = "Full Time";
+    $scope.streetAddress = "";
+    $scope.city = "";
+    $scope.state = "";
+    $scope.postalCode = "";
+//    $scope.fullTime = "Full Time"; //WORKING ON RADIO BUTTONS TO BE ADDED IN FIREBASE
 //    $scope.partTime = "Part Time";
 //    $scope.contract = "Contract";
 //    $scope.freelance = "Freelance";
 //    $scope.internship = "Internship";
+    
+    //SETTING PAGE START INDEXING POINT
     $scope.currentPage = 0;
     $scope.pageSize = 5;
     $scope.jobType = []; //.push is for regular arrays/objects you need to use synchronised arrays/objects with angular
     $scope.jobs = {};
-//    
-//    $scope.numberOfPages=function(){
-//        return Math.ceil($scope.jobs.length/$scope.pageSize);                
-//        }
-//        for (var i=0; i<45; i++) {
-//            $scope.jobs.push("Item "+i);
-//        }
-
+    
+    //INCLUDING RADIO BUTTON JOB TYPE TO BE ADDED IN FIRBASE
     $scope.jobType.push({type:"Full Time"});
     $scope.jobType.push({type:"Part Time"});
     $scope.jobType.push({type:"Contract"});
@@ -37,6 +39,14 @@ blocJobs.controller('jobPostCtrl', ['$scope', '$firebaseArray', function($scope,
     $scope.jobType.push({type:"Internship"});
     $scope.selectedType = $scope.jobType[0].type;
     
+//    $scope.numberOfPages=function(){
+//        return Math.ceil($scope.jobs.length/$scope.pageSize);                
+//        }
+//        for (var i=0; i<45; i++) {
+//            $scope.jobs.push("Item "+i);
+//        }
+
+    //SETTING FIREBASE TO myData VARIABLE
     $scope.myData = new Firebase('https://keodo-todo-list.firebaseio.com/Jobs');
     
     $scope.saveJobs = function() {
@@ -44,7 +54,11 @@ blocJobs.controller('jobPostCtrl', ['$scope', '$firebaseArray', function($scope,
         var timestamp = new Date().valueOf();
         $scope.myData.push({jobTitle:$scope.jobTitle,id: timestamp,
                             companyName:$scope.companyName,id: timestamp,
-                            jobDescription:$scope.jobDescription,id: timestamp
+                            jobDescription:$scope.jobDescription,id: timestamp,
+                            streetAddress:$scope.streetAddress,
+                            city:$scope.city,id: timestamp,
+                            state:$scope.state,id: timestamp,
+                            postalCode:$scope.postalCode,id: timestamp
 //                            jobType:$scope.jobType
 //                            fullTime:$scope.fullTime,
 //                            partTime:$scope.partTime,
@@ -56,6 +70,10 @@ blocJobs.controller('jobPostCtrl', ['$scope', '$firebaseArray', function($scope,
         $scope.jobTitle = "";
         $scope.companyName = "";
         $scope.jobDescription = "";
+        $scope.streetAddress = "";
+        $scope.city = "";
+        $scope.state = "";
+        $scope.postalCode = "";
         $scope.jobType = [];
 //        $scope.fullTime = "Full Time";
 //        $scope.partTime = "Part Time";
@@ -69,15 +87,5 @@ blocJobs.controller('jobPostCtrl', ['$scope', '$firebaseArray', function($scope,
         $scope.$apply();
     });
     
-    // REMOVE JOB ITEM METHOD
-    $scope.removeJob = function (index, job) {
-        
-        // CHECK THAT JOB IS VALID
-        if (job.id === undefined)return;
-
-        // FIREBASE: REMOVE JOB FROM LIST
-        $scope.myData.$remove(job);
-
-    };  
 
 }]);
